9f9c85034995252e35e6c9da04f00ebca174b532
Tran Thi Phuong <phuong.tt@sutrixmedia1.com>
2018-05-30 10:17:20 +0700

    Push code moi len ne

--- src/test/java/utils/Calculator.java
+++ src/test/java/utils/Calculator.java
@@ -4,10 +4,11 @@ public class Calculator {
 	private static int value = 0;
 	private static int x = 100;
 	private static int y = 0;
+	// Author: Phuong Tran
 
 	public static void calculate1(String valueInput) {
 		// Do function in here
-		// Exercise 1
+		// Exercise 1 
 		// Case 1: "20+-*/80"
 		String operators[] = valueInput.split("[0-9]+"); // Split all numbers
 		String inputs[] = valueInput.split("[+-/*//]"); // Split all +-*/
@@ -37,6 +38,7 @@ public class Calculator {
 		}
 	}
 
+
 	// Case 2: "20+-*/     80"
 	public static void calculate2(String valueInput) {
 		valueInput = valueInput.replaceAll("\\s+", ""); // Remove all space and keep all characters
@@ -49,6 +51,8 @@ public class Calculator {
 		calculate1(valueInput);
 	}
 
+
+
 	// Case 4: "20dsdfggdfgd +-*/   fhdfhfdhfdhfdh80"
 	public static void calculate4(String valueInput) {
 		valueInput = valueInput.replaceAll("[^0-9.+-/*//]", ""); // Remove all characters and keep numbers and +-*/																	
@@ -57,7 +61,7 @@ public class Calculator {
 
 	// Exercise: 20+-*/80
 	//public static void calculate5(int numberOne, String operator, int numberTwo) {
-	
+
 	public static void calculate5(int numberOne, String operator , int numberTwo) {
 		switch (operator) {
 		case "+":
